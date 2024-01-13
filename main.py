69d1f97f9ed328bf86aa6c3ffe21f2c8ab1ba273
Maurizio Cardoso Loria <147722037+MCardosoLoria@users.noreply.github.com>
2024-01-13 13:49:24 +0000

    Add files via upload
    tenth commit

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ import codecs
 import awscli
 import re
 
+## Docstrings
+## Split files and imports
+## Add self to code
+
 DATABASE_TYPE = 'postgresql'
 DBAPI = 'psycopg2'
 HOST = 'localhost'
@@ -120,7 +124,7 @@ class DataExtractor:
 
     def extract_from_json_dates():
         s3 = boto3.client("s3")
-        dates_list = s3.download_file("data-handling-public", "date_details.json", r"Multinational Retail Data\sales_data\Multination_date_events.csv")
+        dates_list = s3.download_file("data-handling-public", "date_details.json", r"Multinational Retail Data\sales_data\Multination_date_events.json")
         dates_list = pd.DataFrame(dates_list)
         return dates_list
 
@@ -130,10 +134,9 @@ class DataCleaning:
 
     def clean_user_data(table_name):
         df = DataExtractor.read_rds_table(table_name)
+        df = df.loc[df['country'].isin(['Germany', 'United Kingdom', 'United States'])]
         df.to_csv(r"Multinational Retail Data\sales_data\Multination_legacy_users.csv")
-        read_df = pd.read_csv(r"Multinational Retail Data\sales_data\Multination_legacy_users.csv")
-        read_df = read_df.dropna()
-        return read_df
+        return df
     
     # Removes NULL values from PDF and returns a Pandas Dataframe.
         
@@ -151,36 +154,84 @@ class DataCleaning:
         df = DataExtractor.retrieve_stores_data()
         df = ast.literal_eval(df)
         df = pd.DataFrame(df)
-        df.to_csv(r"Multinational Retail Data\sales_data\Multination_stores_data.csv", index=False, header=True)
-        read_df = pd.read_csv(r"Multinational Retail Data\sales_data\Multination_stores_data.csv")
-        read_df = read_df.dropna()
-        read_df = pd.concat(df, ignore_index = True)
-        return read_df
+        df = pd.concat(df, ignore_index = True)
+        df.to_csv(r"Multinational Retail Data\sales_data\Multination_stores_data.csv")
+        df = pd.read_csv(r"Multinational Retail Data\sales_data\Multination_stores_data.csv")
+        df = df.loc[df['store_type'].isin(['Local', 'Super Store', 'Mall Kiosk', 'Outlet'])]
+        df.to_csv(r"Multinational Retail Data\sales_data\Multination_stores_data.csv")
+        return df
     
     # Removes NULL values from orders table and returns a Pandas Dataframe.
     
     def clean_orders_data(table_name):
         df = DataExtractor.read_rds_table(table_name)
+        df = df.drop(columns = ['first_name', 'last_name', '1', 'level_0'])
         df.to_csv(r"Multinational Retail Data\sales_data\Multination_orders_table.csv")
-        read_df = pd.read_csv(r"Multinational Retail Data\sales_data\Multination_orders_table.csv")
-        read_df = read_df.dropna()
-        read_df = read_df.drop(columns = ['first_name', 'last_name', '1', 'level_0'])
-        return read_df
+        return df
     
+    def multipack_conversion(x):
+        if 'x' in x:
+            a, b, c = x.split(' ')
+            c = c.replace('g', '')
+            x = int(a) * int(c)
+            return x
+        else:
+            return x
+        
+        # ^ = starts with
+        # ? = 0 or one occurence
+        # \d = where the string contains digits
+        # * = zero or more occurences
+        # \w = contains word characters
+
+    def divide_by_1000(x):
+        regex = re.compile(r"^(?P<numbers>\d*\.\d*|\d+)(?P<letters>\w*)$")
+        x = str(x)
+        match = regex.search(x)
+        if match:
+            a, b = match.groups()
+            a = float(a)
+            if b == 'kg':
+                return a
+            if b == 'g' or 'ml':
+                return a / 1000
+        else:
+            return None
+
     def clean_products_data():
         read_df = pd.read_csv(r"Multinational Retail Data\sales_data\Multinational_products.csv")
         read_df = read_df.dropna()
-        read_df['weight'] = read_df['weight'].astype(str)
-        def find_num():
-            [int(num) for num in re.findall(r'\d+', read_df['weight']) if num.isdigit()]
-        read_df['weight_split'] = read_df['weight'].apply(find_num())
+
+        # Drops incorrect rows - find them by funnelling through categorical columns with only a few variations
+        read_df = read_df.loc[read_df['removed'].isin(['Still_avaliable', 'Removed'])]
+        
+        # Creates Unit column
+        read_df['weight_unit'] = read_df['weight'].apply(lambda x: re.findall('[a-z]{0,2}$', x))
+        read_df['weight_unit'] = read_df['weight_unit'].apply(lambda x: x[0])
+
+        # Converts multipacks to one integer value
+        read_df['weight'] = read_df['weight'].apply(lambda x: DataCleaning.multipack_conversion(x))
+
+        # Creates Weight column
+        read_df['weight'] = read_df['weight'].apply(lambda x: DataCleaning.divide_by_1000(x))
+
+        # Changes unit values to kg
+        read_df['weight_unit'] = read_df['weight_unit'].apply(lambda x: x.replace('g', 'kg'))
+        read_df['weight_unit'] = read_df['weight_unit'].apply(lambda x: x.replace('ml', 'kg'))
+        read_df['weight_unit'] = read_df['weight_unit'].apply(lambda x: x.replace('kk', 'k'))
+
+        # Writes to .csv
         read_df.to_csv(r"Multinational Retail Data\product_data_Cleaned.csv", index = False, header = True)
         return read_df
-    
+
     def clean_dates_data():
-        read_df = pd.read_csv(r"Multinational Retail Data\sales_data\Multination_date_events.csv")
+        read_df = pd.read_json(r"Multinational Retail Data\sales_data\Multination_date_events.json")
         read_df = read_df.dropna()
+        read_df.to_csv(r"Multinational Retail Data\date_time_cleaned.csv", index = False, header = True)
+        read_df = pd.read_csv(r"Multinational Retail Data\date_time_cleaned.csv")
+        read_df = read_df.loc[read_df['month'].isin(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])]
+        read_df.to_csv(r"Multinational Retail Data\date_time_cleaned.csv", index = False, header = True)
         return read_df
 
-print(DataCleaning.clean_products_data())
 
+DataCleaning.clean_store_data()
